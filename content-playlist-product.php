f7e6bf92889d5e1cd85a55a7fd6b340818728dbb
Max Harris <maxreuven@gmail.com>
2018-12-30 21:55:26 -0800

    got players workinggit add .! submitting to client

--- content-playlist-product.php
+++ content-playlist-product.php
@@ -37,9 +37,14 @@ if ( empty( $product ) || ! $product->is_visible() ) {
 
 			$thumbnail_size = 'thumbnail';
 
-			woocommerce_template_loop_product_link_open();
-			echo woocommerce_get_product_thumbnail( $thumbnail_size, true );
-			woocommerce_template_loop_product_link_close();
+			// woocommerce_template_loop_product_link_open();
+			// echo woocommerce_get_product_thumbnail( $thumbnail_size, true );
+			// woocommerce_template_loop_product_link_close();
+
+			// Get the player
+			// do_action( 'woocommerce_after_shop_loop_item' );
+			do_action('woocommerce_before_shop_loop_item_title');
+
 		?>
 	</div>
 	<div class="title-cell">
